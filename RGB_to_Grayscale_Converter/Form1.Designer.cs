65112cfaf08207d77dbb9a4da263856eb5d358d6
Lenovo <Lenovo@DESKTOP-NND0PSN>
2023-12-29 15:17:12 +0530

    Add project files.

---
+++ RGB_to_Grayscale_Converter/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace RGB_to_Grayscale_Converter
+{
+    partial class Converter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            this.openBtn = new System.Windows.Forms.Button();
+            this.convertBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox
+            // 
+            this.imageBox.Location = new System.Drawing.Point(25, 27);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(1430, 700);
+            this.imageBox.TabIndex = 0;
+            this.imageBox.TabStop = false;
+            // 
+            // openBtn
+            // 
+            this.openBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openBtn.Location = new System.Drawing.Point(346, 733);
+            this.openBtn.Name = "openBtn";
+            this.openBtn.Size = new System.Drawing.Size(173, 74);
+            this.openBtn.TabIndex = 1;
+            this.openBtn.Text = "Open RGB Image";
+            this.openBtn.UseVisualStyleBackColor = true;
+            this.openBtn.Click += new System.EventHandler(this.openBtn_Click);
+            // 
+            // convertBtn
+            // 
+            this.convertBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convertBtn.Location = new System.Drawing.Point(629, 733);
+            this.convertBtn.Name = "convertBtn";
+            this.convertBtn.Size = new System.Drawing.Size(173, 74);
+            this.convertBtn.TabIndex = 2;
+            this.convertBtn.Text = "Convert to GrayScale";
+            this.convertBtn.UseVisualStyleBackColor = true;
+            this.convertBtn.Click += new System.EventHandler(this.convertBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveBtn.Location = new System.Drawing.Point(918, 733);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(173, 74);
+            this.saveBtn.TabIndex = 3;
+            this.saveBtn.Text = "Save Image";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "PNG File |*.png; |JPEG File |*.jpeg; | JPG File |*.jpg; |All Image Files |*.png; " +
+    "*.jpg;*.jpeg; ";
+            this.openFileDialog1.Title = "Open RGB Image";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "PNG File |*.png; |JPEG File |*.jpeg; | JPG File |*.jpg; |All Image Files |*.png; " +
+    "*.jpg;*.jpeg; ";
+            this.saveFileDialog1.Title = "Save Grayscale Image";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Converter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1481, 813);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.convertBtn);
+            this.Controls.Add(this.openBtn);
+            this.Controls.Add(this.imageBox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Converter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RGB to Grayscale Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imageBox;
+        private System.Windows.Forms.Button openBtn;
+        private System.Windows.Forms.Button convertBtn;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ RGB_to_Grayscale_Converter/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RGB_to_Grayscale_Converter
+{
+    public partial class Converter : Form
+    {
+        public Converter()
+        {
+            InitializeComponent();
+        }
+
+        private void openBtn_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            imageBox.ImageLocation = openFileDialog1.FileName;
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            imageBox.Image.Save(saveFileDialog1.FileName);
+        }
+
+        private void convertBtn_Click(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(imageBox.Image);
+            Bitmap newImg = new Bitmap(imageBox.Image.Width, imageBox.Image.Height);
+
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color colour = img.GetPixel(i, j);
+                    int gray = (int)((colour.R * 0.299) + (colour.G * 0.587) + (colour.B * 0.114));
+                    Color newColour = Color.FromArgb(colour.A, gray, gray, gray);
+                    newImg.SetPixel(i, j, newColour);
+                }
+            }
+            imageBox.Image = newImg;
+        }
+    }
+}
